826eeeba815b5a09b356da01470fc6f831ba2608
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-03-21 17:44:33 -0400

    update path and str

--- GaussNBO/app.py
+++ GaussNBO/app.py
@@ -1,5 +1,6 @@
 # import libs
 import os
+from pathlib import Path
 # local
 from .config import (
     __version__, __author__)
@@ -7,15 +8,15 @@ from .docs import (
     NBOParser
 )
 
-def launch(file_path: str, view_browser: bool = True) -> None:
+def launch(file_path: Path | str, view_browser: bool = True) -> None:
     '''
     Launch to parse Gaussian NBO output file (NBO file) and display the data in a web browser.
     This function uses the NBOParser class to extract data from the NBO file and display it in a web browser.
 
     Parameters
     ----------
-    file_path : str
-        path to the NBO output file
+    file_path : Path | str
+        path to the NBO output file or NBO file content
     view_browser : bool, optional
         If True, the extracted data will be displayed in a web browser. Default is True.
 
@@ -33,9 +34,16 @@ def launch(file_path: str, view_browser: bool = True) -> None:
         if not file_path.endswith('.log'):
             raise Exception("Invalid file format, NBO file required!")
 
-        # check file exist
-        if not os.path.exists(file_path):
-            raise Exception("File not found!, ", file_path)
+        # check
+        if not isinstance(file_path, (str, Path)):
+            raise Exception("Invalid file type, str or Path required!")
+        
+        # SECTION
+        # check file path
+        if isinstance(file_path, Path):
+            # check file exist
+            if not os.path.exists(file_path):
+                raise Exception("File not found!, ", file_path)
 
         # NOTE: init NBOParser
         nbo_parser = NBOParser(file_path)
